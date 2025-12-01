c9c3a599b828525ab0ccaf585b23e5cd9044d162
JauniKappauni <social.jauni@gmail.com>
2025-12-01 09:54:36 +0100

    feat: add player movement

--- main.cpp
+++ main.cpp
@@ -3,14 +3,27 @@
 int screenWidth = 800;
 int screenHeight = 400;
 
+float posX = 350.0f;
+
+float speed = 100.0f;
+
 int main()
 {
     InitWindow(screenWidth, screenHeight, "Arkanoid");
     while (!WindowShouldClose())
     {
+        float dt = GetFrameTime();
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawLine(350, 390, 450, 390, WHITE);
+        DrawLine(posX, 390, posX + 100, 390, WHITE);
+        if (IsKeyDown(KEY_A) && posX > 0)
+        {
+            posX -= speed * dt;
+        }
+        if (IsKeyDown(KEY_D) && posX < screenWidth - 100)
+        {
+            posX += speed * dt;
+        }
         EndDrawing();
     }
     CloseWindow();
